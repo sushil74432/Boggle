a7192b581c05d5aafb725d667d62648679662645
sushil74432 <sushil.techguy@gmail.com>
2020-05-08 14:53:53 +0545

    GA code update

--- app/Views/index.php
+++ app/Views/index.php
@@ -12,6 +12,17 @@
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
 		<link rel="stylesheet" href="assets/main.css">
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-26371590-2"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-26371590-2');
+		</script>
+
 
 		<script>
 			const solveUrl = "<?php echo base_url('ajax');?>";
